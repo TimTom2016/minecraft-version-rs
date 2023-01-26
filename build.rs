6861b39586ab902158bd65711deef97477c51a44
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-01-26 20:57:51 +0900

    generate on build

---
+++ build.rs
@@ -0,0 +1,87 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufWriter;
+use chrono::Utc;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct VersionCollection {
+    versions: Vec<Version>
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Version {
+    id: String,
+    release_time: chrono::DateTime<Utc>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut col = reqwest::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+        .await?
+        .json::<VersionCollection>()
+        .await?
+        .versions;
+
+    col.sort_by_key(|x| x.release_time);
+
+    let f = File::options().create(true).write(true).append(false).open("./src/gen.rs")?;
+    let mut bw = BufWriter::new(f);
+    use std::io::Write;
+
+    writeln!(&mut bw, "#[non_exhaustive]")?;
+    writeln!(&mut bw, "#[allow(non_camel_case_types)]")?;
+    writeln!(&mut bw, "#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]")?;
+    writeln!(&mut bw, "pub enum MinecraftVersion {{")?;
+    let make = |s: &String| s.replace(['.', '-', ' '], "_");
+    let s = col
+        .iter()
+        .map(|x|
+            format!("    _{v},\n", v = make(&x.id))
+        )
+        .collect::<Vec<_>>()
+        .join("");
+    write!(&mut bw, "{}", s)?;
+    writeln!(&mut bw, "}}")?;
+
+    writeln!(&mut bw)?;
+
+    writeln!(&mut bw, "impl core::fmt::Display for MinecraftVersion {{")?;
+    writeln!(&mut bw, "    #[allow(clippy::too_many_lines)]")?;
+    writeln!(&mut bw, "    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {{")?;
+    writeln!(&mut bw, "        let s = match self {{")?;
+    let s = col
+        .iter()
+        .map(|x| format!("            Self::_{case} => \"{value}\", \n", case = make(&x.id), value = &x.id))
+        .collect::<Vec<_>>()
+        .join("");
+    write!(&mut bw, "{}", s)?;
+    writeln!(&mut bw, "        }};")?;
+    writeln!(&mut bw, "    ")?;
+    writeln!(&mut bw, "    f.write_str(s)")?;
+    writeln!(&mut bw, "    }}")?;
+    writeln!(&mut bw, "}}")?;
+
+    writeln!(&mut bw)?;
+
+    writeln!(&mut bw, "impl core::str::FromStr for MinecraftVersion {{")?;
+    writeln!(&mut bw, "    type Err = ();")?;
+    writeln!(&mut bw, "    #[allow(clippy::too_many_lines)]")?;
+    writeln!(&mut bw, "    fn from_str(s: &str) -> Result<Self, Self::Err> {{")?;
+    writeln!(&mut bw, "        match s {{")?;
+
+    let s = col
+        .iter()
+        .map(|x| format!("            \"{ver}\" => Ok(Self::_{variant}),\n", ver = &x.id, variant = make(&x.id)))
+        .collect::<Vec<_>>()
+        .join("");
+
+    write!(&mut bw, "{}", s)?;
+    write!(&mut bw, "            _ => Err(()),")?;
+    writeln!(&mut bw, "        }}")?;
+    writeln!(&mut bw, "    }}")?;
+    writeln!(&mut bw, "}}")?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![forbid(unsafe_code)]
+#![deny(clippy::all, clippy::cargo)]
+#![warn(clippy::pedantic, clippy::nursery)]
+
+mod gen;
+
+pub use gen::MinecraftVersion;
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn ord_is_release_order() {
+        assert!(MinecraftVersion::_1_19_3 > MinecraftVersion::_1_19_2);
+    }
+
+    #[test]
+    fn eq_reflexibity() {
+        assert!(MinecraftVersion::_1_19_3 == MinecraftVersion::_1_19_3);
+    }
+
+    #[test]
+    fn from_str_takes_version() {
+        assert!(MinecraftVersion::from_str("1.19.3").unwrap() == MinecraftVersion::_1_19_3);
+    }
+
+    #[test]
+    fn version_is_display() {
+        assert_eq!(MinecraftVersion::_1_19_3.to_string(), "1.19.3".to_string());
     }
 }
