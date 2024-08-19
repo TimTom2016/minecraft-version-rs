c705f8e51a08140a6f14c17a7d5a439af936bf4f
timtom2016 <dev@timtom2016.com>
2024-08-19 22:30:20 +0200

    Changed Code to a macro(quote) approach

--- build.rs
+++ build.rs
@@ -1,14 +1,15 @@
+use chrono::Utc;
+use quote::quote;
+use serde::Deserialize;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufWriter;
-use chrono::Utc;
-use serde::Deserialize;
+use std::io::Write;
 
 #[derive(Deserialize)]
 struct VersionCollection {
-    versions: Vec<Version>
+    versions: Vec<Version>,
 }
-
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Version {
@@ -23,65 +24,71 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         .json::<VersionCollection>()
         .await?
         .versions;
-
     col.sort_by_key(|x| x.release_time);
+    let versions = col
+        .iter()
+        .map(|version| {
+            let mut id = version.id.replace(['.', '-', ' '], "_");
+            id.insert(0, '_');
+            let id: proc_macro2::TokenStream = id.parse().unwrap();
+            quote! {#id}
+        })
+        .collect::<Vec<_>>();
+    let version_strings = col
+        .iter()
+        .map(|version| {
+            let mut id = version.id.replace(['.', '-', ' '], "_");
+            id.insert(0, '_');
+            let id: proc_macro2::TokenStream = id.parse().unwrap();
+            let id2 = version.id.clone();
+            quote! {Self::#id => #id2}
+        })
+        .collect::<Vec<_>>();
+    let strings_versions = col
+        .iter()
+        .map(|version| {
+            let mut id = version.id.replace(['.', '-', ' '], "_");
+            id.insert(0, '_');
+            let id: proc_macro2::TokenStream = id.parse().unwrap();
+            let id2 = version.id.clone();
+            quote! {#id2=>  Ok(Self::#id)}
+        })
+        .collect::<Vec<_>>();
+    let tokens = quote! {
+        #[non_exhaustive]
+        #[allow(non_camel_case_types)]
+        #[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+        pub enum MinecraftVersion {
+            #(#versions),*
+        }
 
-    let f = File::options().create(true).write(true).append(false).open("./src/gen.rs")?;
+        impl core::fmt::Display for MinecraftVersion {
+            #[allow(clippy::too_many_lines)]
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                let s = match self {
+                    #(#version_strings),*
+                };
+                f.write_str(s)
+            }
+        }
+        impl core::str::FromStr for MinecraftVersion {
+            type Err = ();
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s {
+                    #(#strings_versions),*,
+                    _ => Err(())
+                }
+            }
+        }
+    };
+
+    let syntax_tree = syn::parse2(tokens).unwrap();
+    let f = File::options()
+        .create(true)
+        .write(true)
+        .append(false)
+        .open("./src/gen.rs")?;
     let mut bw = BufWriter::new(f);
-    use std::io::Write;
-
-    writeln!(&mut bw, "#[non_exhaustive]")?;
-    writeln!(&mut bw, "#[allow(non_camel_case_types)]")?;
-    writeln!(&mut bw, "#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]")?;
-    writeln!(&mut bw, "pub enum MinecraftVersion {{")?;
-    let make = |s: &String| s.replace(['.', '-', ' '], "_");
-    let s = col
-        .iter()
-        .map(|x|
-            format!("    _{v},\n", v = make(&x.id))
-        )
-        .collect::<Vec<_>>()
-        .join("");
-    write!(&mut bw, "{}", s)?;
-    writeln!(&mut bw, "}}")?;
-
-    writeln!(&mut bw)?;
-
-    writeln!(&mut bw, "impl core::fmt::Display for MinecraftVersion {{")?;
-    writeln!(&mut bw, "    #[allow(clippy::too_many_lines)]")?;
-    writeln!(&mut bw, "    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {{")?;
-    writeln!(&mut bw, "        let s = match self {{")?;
-    let s = col
-        .iter()
-        .map(|x| format!("            Self::_{case} => \"{value}\", \n", case = make(&x.id), value = &x.id))
-        .collect::<Vec<_>>()
-        .join("");
-    write!(&mut bw, "{}", s)?;
-    writeln!(&mut bw, "        }};")?;
-    writeln!(&mut bw, "    ")?;
-    writeln!(&mut bw, "    f.write_str(s)")?;
-    writeln!(&mut bw, "    }}")?;
-    writeln!(&mut bw, "}}")?;
-
-    writeln!(&mut bw)?;
-
-    writeln!(&mut bw, "impl core::str::FromStr for MinecraftVersion {{")?;
-    writeln!(&mut bw, "    type Err = ();")?;
-    writeln!(&mut bw, "    #[allow(clippy::too_many_lines)]")?;
-    writeln!(&mut bw, "    fn from_str(s: &str) -> Result<Self, Self::Err> {{")?;
-    writeln!(&mut bw, "        match s {{")?;
-
-    let s = col
-        .iter()
-        .map(|x| format!("            \"{ver}\" => Ok(Self::_{variant}),\n", ver = &x.id, variant = make(&x.id)))
-        .collect::<Vec<_>>()
-        .join("");
-
-    write!(&mut bw, "{}", s)?;
-    write!(&mut bw, "            _ => Err(()),")?;
-    writeln!(&mut bw, "        }}")?;
-    writeln!(&mut bw, "    }}")?;
-    writeln!(&mut bw, "}}")?;
-
+    write!(&mut bw, "{}", prettyplease::unparse(&syntax_tree))?;
     Ok(())
 }
